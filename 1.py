3923ecf9d7a60989bc07cb8e25a92de957d5dfcc
lubani <lubani@ac.sce.ac.il>
2024-01-05 01:36:38 +0200

    First commit

---
+++ 1.py
@@ -0,0 +1,39 @@
+"""
+Luba Ira
+ID 31264010
+email lubacareer@gmail.com
+"""
+
+first = 7
+second = 44.3
+print(first + second)
+print(first * second)
+print(first / second)
+print(second / first)
+
+a = 8
+a = 17
+a = 9
+b = 6
+c = a + b
+b = c + a
+b = 8
+
+print(f"a = {a}, b = {b}, c = {c}")
+
+print("C. There is no practical difference between \' and \", "
+      "The interpreter treats it the same - string literals.")
+
+print("You cannot concatenate a string and an integer "
+      "in a print statement with a plus.")
+print("You can insert a comma instead of a plus, or convert to an integer.")
+
+x = 5
+y = 2.36
+print(x+int(y))
+print("D. The answer would be 7.")
+a = 8
+b = "123"
+print(a + int(b))
+
+
---
+++ 2.py
@@ -0,0 +1,81 @@
+from operator import concat
+
+class Task2:
+
+    def printHello(self):
+        print("Hello")
+
+    def calculate(self):
+        print("5 + 3.2 = ", 5 + 3.2)
+
+    def my_name(self, name):
+        print(name)
+
+    def div_two(self, a):
+        print(f"{a} / 2 = ", a / 2)
+
+    def add(self, a, b):
+        return a + b
+
+    def concatenate(self, a, b):
+        return concat(a, b)
+
+
+if __name__ == "__main__":
+    print("A. ")
+    X, Y = input("Please enter integers for X and Y, separated by spaces: ").strip().split(' ')
+    if X > Y:
+        print("BIG")
+    elif X < Y:
+        print("small")
+    else:
+        pass
+
+    print("B. ")
+    for i in range(5):
+        print(i)
+
+    print("C. ")
+    A = input("Please enter a number between 1 and 4: ")
+    match A:
+        case "1":
+            print("Summer")
+        case "2":
+            print("Winter")
+        case "3":
+            print("Fall")
+        case "4":
+            print("Spring")
+        case _:
+            print("Not a number between 1 and 4")
+
+    print("D. ")
+    count = 1
+    while count < 11:
+        print(count)
+        count += 1
+    print(f"The loop ran {count-1} times")
+
+    print("E. ")
+    my_inf = {"age": 30, "letter_lname": 'L', "NIS_dollar": 0.27, "abroad": True, "apt_no": 22}
+    print(my_inf)
+    print("What happens if I sum age and currency?")
+    print(my_inf["age"] + my_inf["NIS_dollar"])
+
+
+    print("F. ")
+    phone_num = input("Please enter a phone number: ")
+    print("Phone Number ", phone_num)
+
+    print("G. ")
+    GHIM = Task2()
+    GHIM.printHello()
+    GHIM.calculate()
+    print("H. ")
+    GHIM.my_name("John")
+    GHIM.div_two(5)
+    print("I. ")
+    print("5 + 5 = ", GHIM.add(5, 5))
+    print(GHIM.concatenate("Hello ", "World"))
+
+
